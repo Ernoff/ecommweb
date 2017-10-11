c026411d7e35838c425334872310a5bb63d164cd
Ernoff <ernest.offiong@gmail.com>
2017-10-11 15:19:09 +0100

    added basic setup for web interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SuiModule } from 'ng2-semantic-ui';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { CartComponent } from './components/cart/cart.component';
+
+
+const appRoutes: Routes = [
+  { path: 'user', component: UserComponent },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  { path: '',
+    component: ProductsComponent
+  },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ProductsComponent,
+    PagenotfoundComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes, 
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
